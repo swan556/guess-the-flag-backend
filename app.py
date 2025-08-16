c907b822f984e25da31342a144095f92bba656ec
swan556 <swanand556@gmail.com>
2025-08-16 15:03:35 +0530

    fixed the port issue

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import random
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -20,4 +21,5 @@ def pickRandomFlag():
         })
 
 if(__name__=="__main__"):
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
